183bdffbc1c1bd7fba89966f6c744af0e4b1a44d
René Kijewski <rene.kijewski@fu-berlin.de>
2012-04-02 12:23:13 +0200

    quotes don't need the BEGIN

--- core/quotes.c
+++ core/quotes.c
@@ -1,152 +1,151 @@
 #include  "quotes.h"
 #include <round.h>
 
-#define BEGIN "\e\xA8"
-#define CHAOS "\e\x8C chaOS "BEGIN
+#define CHAOS "\e\x8C chaOS \e\xA8"
 
 const char *chaos_quotes[][2] = {
-  { BEGIN"There is much "CHAOS" in me, much "CHAOS" in our time.",
+  { "There is much "CHAOS" in me, much "CHAOS" in our time.",
     "Otto Dix" },
 
-  { BEGIN"All genius is a conquering of "CHAOS" and mystery.",
+  { "All genius is a conquering of "CHAOS" and mystery.",
     "Otto Weininger" },
 
-  { BEGIN"Any change is resisted because bureaucrats have a vested interest in the "CHAOS" in which they exist.",
+  { "Any change is resisted because bureaucrats have a vested interest in the "CHAOS" in which they exist.",
     "Richard M. Nixon" },
 
-  { BEGIN"Anything you build on a large scale or with intense passion invites "CHAOS".",
+  { "Anything you build on a large scale or with intense passion invites "CHAOS".",
     "Francis Ford Coppola" },
 
-  { BEGIN"Art is the triumph over "CHAOS".",
+  { "Art is the triumph over "CHAOS".",
     "John Cheever" },
 
-  { BEGIN"Art, in itself, is an attempt to bring order out of "CHAOS".",
+  { "Art, in itself, is an attempt to bring order out of "CHAOS".",
     "Stephen Sondheim" },
 
-  { BEGIN"Blame is just a lazy person's way of making sense of "CHAOS".",
+  { "Blame is just a lazy person's way of making sense of "CHAOS".",
     "Doug Coupland" },
 
-  { BEGIN""CHAOS" in the midst of "CHAOS" isn't funny, but "CHAOS" in the midst of order is.",
+  { CHAOS" in the midst of "CHAOS" isn't funny, but "CHAOS" in the midst of order is.",
     "Steve Martin" },
 
-  { BEGIN""CHAOS" in the world brings uneasiness, but it also allows the opportunity for creativity and growth.",
+  { CHAOS" in the world brings uneasiness, but it also allows the opportunity for creativity and growth.",
     "Tom Barrett" },
 
-  { BEGIN""CHAOS" is a friend of mine.",
+  { CHAOS" is a friend of mine.",
     "Bob Dylan" },
 
-  { BEGIN""CHAOS" is a name for any order that produces confusion in our minds.",
+  { CHAOS" is a name for any order that produces confusion in our minds.",
     "George Santayana" },
 
-  { BEGIN""CHAOS" is inherent in all compounded things. Strive on with diligence.",
+  { CHAOS" is inherent in all compounded things. Strive on with diligence.",
     "Buddha" },
 
-  { BEGIN""CHAOS" is rejecting all you have learned, "CHAOS" is being yourself.",
+  { CHAOS" is rejecting all you have learned, "CHAOS" is being yourself.",
     "Emile M. Cioran" },
 
-  { BEGIN""CHAOS" often breeds life, when order breeds habit.",
+  { CHAOS" often breeds life, when order breeds habit.",
     "Henry B. Adams" },
 
-  { BEGIN""CHAOS" was the law of nature; Order was the dream of man.",
+  { CHAOS" was the law of nature; Order was the dream of man.",
     "Henry B. Adams" },
 
-  { BEGIN"Civilization begins with order, grows with liberty and dies with "CHAOS".",
+  { "Civilization s with order, grows with liberty and dies with "CHAOS".",
     "Will Durant" },
 
-  { BEGIN"Everything is contingent, and there is also "CHAOS".",
+  { "Everything is contingent, and there is also "CHAOS".",
     "Spalding Gray" },
 
-  { BEGIN"Freedom is just "CHAOS", with better lighting.",
+  { "Freedom is just "CHAOS", with better lighting.",
     "Alan Dean Foster" },
 
-  { BEGIN"I accept "CHAOS", I'm not sure whether it accepts me.",
+  { "I accept "CHAOS", I'm not sure whether it accepts me.",
     "Bob Dylan" },
 
-  { BEGIN"I am still making order out of "CHAOS" by reinvention.",
+  { "I am still making order out of "CHAOS" by reinvention.",
     "John Le Carre" },
 
-  { BEGIN"I believe, as Lenin said, that this revolutionary "CHAOS" may yet crystallize into new forms of life.",
+  { "I believe, as Lenin said, that this revolutionary "CHAOS" may yet crystallize into new forms of life.",
     "Mikhail Gorbachev" },
 
-  { BEGIN"I have great belief in the fact that whenever there is "CHAOS", it creates wonderful thinking. I consider "CHAOS" a gift.",
+  { "I have great belief in the fact that whenever there is "CHAOS", it creates wonderful thinking. I consider "CHAOS" a gift.",
     "Septima Clark" },
 
-  { BEGIN"I want to create a little "CHAOS" and make people's heads turn.",
+  { "I want to create a little "CHAOS" and make people's heads turn.",
     "Scott Caan" },
 
-  { BEGIN"I'm a study of a man in "CHAOS" in search of frenzy.",
+  { "I'm a study of a man in "CHAOS" in search of frenzy.",
     "Oscar Levant" },
 
-  { BEGIN"I'm definitely more attracted to "CHAOS" than to order.",
+  { "I'm definitely more attracted to "CHAOS" than to order.",
     "Connie Nielsen" },
 
-  { BEGIN"I'm not afraid of "CHAOS" and I'm happy talking to strangers.",
+  { "I'm not afraid of "CHAOS" and I'm happy talking to strangers.",
     "Fiona Shaw" },
 
-  { BEGIN"If we wish to make a new world we have the material ready. The first one, too, was made out of "CHAOS".",
+  { "If we wish to make a new world we have the material ready. The first one, too, was made out of "CHAOS".",
     "Robert Quillen" },
 
-  { BEGIN"In all "CHAOS" there is a cosmos, in all disorder a secret order.",
+  { "In all "CHAOS" there is a cosmos, in all disorder a secret order.",
     "Carl Jung" },
 
-  { BEGIN"In the midst of movement and "CHAOS", keep stillness inside of you.",
+  { "In the midst of movement and "CHAOS", keep stillness inside of you.",
     "Deepak Chopra" },
 
-  { BEGIN"Invention, it must be humbly admitted, does not consist in creating out of void, but out of "CHAOS".",
+  { "Invention, it must be humbly admitted, does not consist in creating out of void, but out of "CHAOS".",
     "Mary Wollstonecraft Shelley" },
 
-  { BEGIN"It is only a step from boredom to disillusionment, which leads naturally to self-pity, which in turn ends in "CHAOS".",
+  { "It is only a step from boredom to disillusionment, which leads naturally to self-pity, which in turn ends in "CHAOS".",
     "Manly Hall" },
 
-  { BEGIN"Living in continual "CHAOS" is exhausting, frightening. The catch is that it's also very addictive.",
+  { "Living in continual "CHAOS" is exhausting, frightening. The catch is that it's also very addictive.",
     "Lorna Luft" },
 
-  { BEGIN"No one welcomes "CHAOS", but why crave stability and predictability?",
+  { "No one welcomes "CHAOS", but why crave stability and predictability?",
     "Hugh Mackay" },
 
-  { BEGIN"Oh, God, why don't I remember that a little "CHAOS" is good for the soul?",
+  { "Oh, God, why don't I remember that a little "CHAOS" is good for the soul?",
     "Marilyn French" },
 
-  { BEGIN"One must still have "CHAOS" in oneself to be able to give birth to a dancing star.",
+  { "One must still have "CHAOS" in oneself to be able to give birth to a dancing star.",
     "Friedrich Nietzsche" },
 
-  { BEGIN"Organized religion preaches Order and Love but spawns "CHAOS" and Fury. Why?",
+  { "Organized religion preaches Order and Love but spawns "CHAOS" and Fury. Why?",
     "Kerry Thornley" },
 
-  { BEGIN"Out of "CHAOS" God made a world, and out of high passions comes a people.",
+  { "Out of "CHAOS" God made a world, and out of high passions comes a people.",
     "Lord Byron" },
 
-  { BEGIN"Progress everywhere today does seem to come so very heavily disguised as "CHAOS".",
+  { "Progress everywhere today does seem to come so very heavily disguised as "CHAOS".",
     "Joyce Grenfell" },
 
-  { BEGIN"The irreversibility of time is the mechanism that brings order out of "CHAOS".",
+  { "The irreversibility of time is the mechanism that brings order out of "CHAOS".",
     "Ilya Prigogine" },
 
-  { BEGIN"The task of art today is to bring "CHAOS" into order.",
+  { "The task of art today is to bring "CHAOS" into order.",
     "Theodor Adorno" },
 
-  { BEGIN"The world is "CHAOS". Nothingness is the yet-to-be-born god of the world.",
+  { "The world is "CHAOS". Nothingness is the yet-to-be-born god of the world.",
     "Georg Buchner" },
 
-  { BEGIN"The World, form'd out of "CHAOS". Man is made.",
+  { "The World, form'd out of "CHAOS". Man is made.",
     "George Sandys" },
 
-  { BEGIN"We adore "CHAOS" because we love to produce order.",
+  { "We adore "CHAOS" because we love to produce order.",
     "M. C. Escher" },
 
-  { BEGIN"We have arrived at an intellectual "CHAOS".",
+  { "We have arrived at an intellectual "CHAOS".",
     "Aleksandr Solzhenitsyn" },
 
-  { BEGIN"We live in a rainbow of "CHAOS".",
+  { "We live in a rainbow of "CHAOS".",
     "Paul Cezanne" },
 
-  { BEGIN"When tempest tossed, embrace "CHAOS".",
+  { "When tempest tossed, embrace "CHAOS".",
     "Dean Koontz" },
 
-  { BEGIN"When the mind withdraws into itself and dispenses with facts it makes only "CHAOS".",
+  { "When the mind withdraws into itself and dispenses with facts it makes only "CHAOS".",
     "Edith Hamilton" },
 
-  { BEGIN"You can reconstruct the picture from "CHAOS" and memory's ruins.",
+  { "You can reconstruct the picture from "CHAOS" and memory's ruins.",
     "Kay Boyle" },
 };
 
