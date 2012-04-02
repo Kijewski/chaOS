cbe48a3bcdb0b137e5373ea434e6ca634591a9bc
René Kijewski <rene.kijewski@fu-berlin.de>
2012-04-02 12:24:36 +0200

    Less predictable random number generator

--- common/random.c
+++ common/random.c
@@ -1,16 +1,52 @@
 #include "random.h"
 #include <spinlock.h>
+#include <round.h>
 #include <devices/rtc.h>
 
 static uint64_t seed[4];
 static spinlock lock;
+
+static inline uint64_t
+rol (uint64_t v)
+{
+  asm ("rolq $7, %0" : "+r"(v));
+  return v;
+}
+
+static inline uint64_t
+rdtsc (void)
+{
+  uint32_t v[2];
+  asm ("rdtsc" : "=a"(v[0]), "=d"(v[1]));
+  return v[0] ^ ((uint64_t) v[1] << 32);
+}
+
+static uint64_t
+random_get_real (void)
+{
+  uint64_t result = seed[0] + seed[1] + seed[2] + seed[3];
+
+  for (unsigned i = 0; i < ARRAY_LEN (seed) - 1; ++i)
+    seed[i] ^= rol (seed[i+1]);
+  seed[ARRAY_LEN (seed) - 1] ^= rol (result);
+
+  return result;
+}
+
+uint64_t
+random_get (void)
+{
+  spinlock_acquire (&lock);
+  uint64_t result = random_get_real ();
+  spinlock_release (&lock);
+  return result ^ rdtsc ();
+}
 
 bool
 random_init (void)
 {
   spinlock_init (&lock);
 
-  // TODO: proper seeds
   seed[0] = 0x0123456780234567ull;
   seed[1] = 0xDEADBEEFDEADBEEFull;
   seed[2] = 0xBAD1337C0FFEEBADull;
@@ -19,35 +55,15 @@ random_init (void)
   if (!rtc_read (&time))
     return false;
 
-  seed[3] = ((uint64_t) time.second <<  0) +
-            ((uint64_t) time.minute <<  8) +
-            ((uint64_t) time.hour   << 16) +
-            ((uint64_t) time.day    << 24) +
-            ((uint64_t) time.month  << 32) +
-            ((uint64_t) time.year   << 40);
+  seed[ARRAY_LEN (seed) - 1] = ((uint64_t) time.second <<  0) +
+                               ((uint64_t) time.minute <<  8) +
+                               ((uint64_t) time.hour   << 16) +
+                               ((uint64_t) time.day    << 24) +
+                               ((uint64_t) time.month  << 32) +
+                               ((uint64_t) time.year   << 40);
+
+  for (unsigned i = 0; i < 2*ARRAY_LEN (seed); ++i)
+    (void) random_get_real (); // mix the numbers
 
   return true;
-}
-
-static inline uint64_t
-rol (uint64_t v)
-{
-  asm volatile ("rolq $7, %0" : "+r"(v));
-  return v;
-}
-
-uint64_t
-random_get (void)
-{
-  spinlock_acquire (&lock);
-
-  uint64_t result = seed[0] + seed[1] + seed[2] + seed[3];
-
-  seed[0] ^= rol (seed[1]);
-  seed[1] ^= rol (seed[2]);
-  seed[2] ^= rol (seed[3]);
-  seed[3] ^= rol (result);
-
-  spinlock_release (&lock);
-  return result;
 }
